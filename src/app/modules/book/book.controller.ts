d80d74c3efb2dde5d56c1941a72c1fcf5e3853e2
MH01-Hasan <mehediph2021@gmail.com>
2023-09-04 01:22:21 -0800

    book

---
+++ src/app/modules/book/book.controller.ts
@@ -0,0 +1,83 @@
+import { Book } from '@prisma/client';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import paginationFields from '../../../constants/pagination';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { BookFilterAbleFileds } from './book.constants';
+import { BookService } from './book.service';
+
+const createBook = catchAsync(async (req: Request, res: Response) => {
+  const result = await BookService.createBook(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book created successful',
+    data: result,
+  });
+});
+const getAllBook = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, BookFilterAbleFileds);
+
+  const paginationOptions = pick(req.query, paginationFields);
+  const result = await BookService.getAllBook(filters, paginationOptions);
+  sendResponse<Book[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book retrieved successful',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+const getSingleBook = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await BookService.getSingleBook(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book retrieved successful',
+    data: result,
+  });
+});
+const getBooksByCategoryId = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const paginationOptions = pick(req.query, paginationFields);
+  const result = await BookService.getBooksByCategoryId(id, paginationOptions);
+  sendResponse<Book[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Books with associated category data fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+const updateSingleBook = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await BookService.updateSingleBook(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book updated successful',
+    data: result,
+  });
+});
+const deleteSingleBook = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await BookService.deleteSingleBook(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Book deleted successful',
+    data: result,
+  });
+});
+
+export const BookController = {
+  createBook,
+  getAllBook,
+  getSingleBook,
+  getBooksByCategoryId,
+  updateSingleBook,
+  deleteSingleBook,
+};
